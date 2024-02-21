af0b5eb2585ffbf9daf540d1798c1c88911c0dfa
kumoSleeping <zjr2992@outlook.com>
2024-02-22 05:32:45 +0800

    core 3.1.1 readme

--- DemoProject2/register/example/__init__.py
+++ DemoProject2/register/example/__init__.py
@@ -11,13 +11,4 @@ def m_w(event: Event):
             event.message_create('汪！')
 
 
-@on.message_created
-def test_pic(event: Event):
-    if res := match_command(event, 'test_pic', arg_less=True):
-        # 打开图片
-        with open('register/example/eg.jpg', 'rb') as f:
-            img = f.read()
-        res.send(h.image(img))
-
-
 
